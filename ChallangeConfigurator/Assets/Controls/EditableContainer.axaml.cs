3f8595d71c9ecdfd6fe7fa72466c30bf424dfcbf
Chris <filmee24@gmail.com>
2023-03-06 11:48:27 +0100

    feat: Add EditableContainer
    To enable to view different controls to edit the additonal information

---
+++ ChallangeConfigurator/Assets/Controls/EditableContainer.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace ChallangeConfigurator.Assets.Controls;
+
+public class EditableContainer : ContentControl
+{
+    public static readonly StyledProperty<bool> IsEditModeProperty =
+        AvaloniaProperty.Register<EditableContainer, bool>(nameof(IsEditMode));
+
+    public static readonly StyledProperty<object> EditModeContentProperty =
+        AvaloniaProperty.Register<EditableContainer, object>(nameof(EditModeContent));
+
+    public bool IsEditMode
+    {
+        get => GetValue(IsEditModeProperty);
+        set => SetValue(IsEditModeProperty, value);
+    }
+    
+    public object EditModeContent
+    {
+        get => GetValue(EditModeContentProperty);
+        set => SetValue(EditModeContentProperty, value);
+    }
+
+    //ToDo: DeleteCommand
+    //ToDo: AcceptCommand
+}
