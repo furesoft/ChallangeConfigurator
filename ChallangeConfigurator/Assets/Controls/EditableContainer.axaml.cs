bceba4bfca5fe9fc3a60624e48238f6aeeed0638
Chris <filmee24@gmail.com>
2023-03-07 12:11:59 +0100

    feat: Add Hiding Delete Button

--- ChallangeConfigurator/Assets/Controls/EditableContainer.axaml.cs
+++ ChallangeConfigurator/Assets/Controls/EditableContainer.axaml.cs
@@ -1,6 +1,6 @@
-﻿using Avalonia;
+﻿using System.Windows.Input;
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
 
 namespace ChallangeConfigurator.Assets.Controls;
 
@@ -9,21 +9,45 @@ public class EditableContainer : ContentControl
     public static readonly StyledProperty<bool> IsEditModeProperty =
         AvaloniaProperty.Register<EditableContainer, bool>(nameof(IsEditMode));
 
+    public static readonly StyledProperty<bool> IsDeleteableProperty =
+        AvaloniaProperty.Register<EditableContainer, bool>(nameof(IsDeleteable), true);
+
     public static readonly StyledProperty<object> EditModeContentProperty =
         AvaloniaProperty.Register<EditableContainer, object>(nameof(EditModeContent));
+
+    public static readonly StyledProperty<object> CommandParameterProperty =
+        AvaloniaProperty.Register<EditableContainer, object>(nameof(CommandParameter));
+
+    public static readonly StyledProperty<ICommand> DeleteCommandProperty =
+        AvaloniaProperty.Register<EditableContainer, ICommand>(nameof(DeleteCommand));
 
     public bool IsEditMode
     {
         get => GetValue(IsEditModeProperty);
         set => SetValue(IsEditModeProperty, value);
     }
-    
+
+    public bool IsDeleteable
+    {
+        get => GetValue(IsDeleteableProperty);
+        set => SetValue(IsDeleteableProperty, value);
+    }
+
+    public ICommand DeleteCommand
+    {
+        get => GetValue(DeleteCommandProperty);
+        set => SetValue(DeleteCommandProperty, value);
+    }
+
     public object EditModeContent
     {
         get => GetValue(EditModeContentProperty);
         set => SetValue(EditModeContentProperty, value);
     }
 
-    //ToDo: DeleteCommand
-    //ToDo: AcceptCommand
+    public object CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
 }
