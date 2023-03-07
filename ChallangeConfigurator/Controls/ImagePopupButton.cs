bc421c7ce9e898268fb28b65498c71bee9c928e3
Chris <filmee24@gmail.com>
2023-03-07 14:32:32 +0100

    feat: Add ImagePopupButton

---
+++ ChallangeConfigurator/Controls/ImagePopupButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Metadata;
+using Material.Icons;
+using ReactiveUI;
+
+namespace ChallangeConfigurator.Controls;
+
+public class ImagePopupButton : ToggleButton
+{
+    public static StyledProperty<MaterialIconKind> KindProperty =
+        AvaloniaProperty.Register<ImagePopupButton, MaterialIconKind>(nameof(Kind));
+
+    public static StyledProperty<object> PopupContentProperty =
+        AvaloniaProperty.Register<ImagePopupButton, object>(nameof(PopupContent));
+
+    public static StyledProperty<string> TextProperty =
+        AvaloniaProperty.Register<ImagePopupButton, string>(nameof(TextProperty));
+
+    public ICommand OpenPopupCommand { get; set; }
+
+    public MaterialIconKind Kind
+    {
+        get => GetValue(KindProperty);
+        set => SetValue(KindProperty, value);
+    }
+    
+    public object PopupContent
+    {
+        get => GetValue(PopupContentProperty);
+        set => SetValue(PopupContentProperty, value);
+    }
+
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public ImagePopupButton()
+    {
+        OpenPopupCommand = ReactiveCommand.Create(OpenPopup);
+    }
+
+    private void OpenPopup()
+    {
+        IsChecked = !IsChecked;
+    }
+}
